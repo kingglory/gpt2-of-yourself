eb0168c7eb3303ad3051210d4fc0a52c51b734ad
Wangwensong <312775704@qq.com>
2023-04-30 20:01:42 +0800

    Add files via upload

---
+++ configs/test.py
@@ -0,0 +1,30 @@
+import os
+
+path = './dataset'
+model_path = path + '/models/'
+
+data = {
+    'path': path + '/test/',
+}
+data = {
+    **data,
+    'raw': data['path'] + 'raw.txt',
+    'raw_cut': data['path'] + 'raw.cut.txt',
+    'vocab': data['path'] + 'vocab.txt',
+    'pickle': data['path'] + 'data.pickle',
+    'pickle_path': os.path.join(data['path'], 'pickle')
+}
+
+model = {
+    'max_length': 512,
+    'n_positions': 512,
+    'n_ctx': 512,
+    'n_embd': 768,
+    'n_layer': 4,
+    'n_head': 4,
+    'batch_size': 8
+}
+
+
+data = type('data', (), data)
+model = type('model', (), model)
